20ae45836395e5ae6de698ff295698b1770d97e5
Bezalel Leyian <158043641+LeyianB24@users.noreply.github.com>
2026-08-06 21:51:46 +0300

    huuyu

--- components/CommandPalette.tsx
+++ components/CommandPalette.tsx
@@ -130,7 +130,7 @@ export default function CommandPalette() {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
-            onClick={() => setIsOpen(false)}
+            onClick={closePalette}
             className="fixed inset-0 bg-black/60 backdrop-blur-sm z-[100]"
           />
 
